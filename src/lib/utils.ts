29a7fc5c2f3bce19b29e60bd7c21f7c469a56881
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2024-04-22 10:11:59 +0200

    chore

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: Date) {
-  intlFormat(date, {
+  return intlFormat(date, {
     locale: "pl",
   });
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -12,6 +14,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["Geist Sans", ...fontFamily.sans],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
